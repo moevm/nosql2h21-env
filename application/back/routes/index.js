f36b54e83ef529af71aa4dfb90f8d548b5273070
a-kuzina <a.m.kuzina@gmail.com>
2021-11-27 21:25:22 +0300

    Changed query for table - added ability to return part of the rows by setting current page number and number of lines on the page

--- application/back/routes/index.js
+++ application/back/routes/index.js
@@ -6,20 +6,25 @@ let creds = require("./extras/credentials.json");
 
 let db_info = require("./extras/db_info");
 
-async function filter_request(states, years) {
+async function filter_request(states, interval, page, lines) {
 
   let driver = neo4j.driver("neo4j://localhost", neo4j.auth.basic(creds.user, creds.password));
   let session = driver.session();
+
+  lines = parseInt(lines);
+  page = parseInt(page);
+  let skip = page*lines;
   try {
       let res = await session.run("MATCH (location:Address)-[observation]->(date:Date) \
-                WHERE location.state IN $state_list AND date.year IN $year_list \
-                RETURN location.address AS address, location.county_code AS county_code, location.city AS city, \
+                WHERE location.state IN $state_list AND date.year >= $min_year AND date.year <= $max_year \
+                RETURN location.address AS address, location.county_code AS county_code, location.city AS city, location.county AS county,\
                 location.state AS state, location.state_code AS state_code, location.site_num AS site_num, date.date_local AS date_local, \
                 observation.firstMH_O3 AS firstMH_O3, observation.firstMV_O3 AS firstMV_O3, observation.mean_O3 AS mean_O3, observation.unit_O3 AS unit_O3, observation.aqi_O3 AS aqi_O3, \
                 observation.firstMH_NO2 AS firstMH_NO2, observation.firstMV_NO2 AS firstMV_NO2, observation.mean_NO2 AS mean_NO2, observation.unit_NO2 AS unit_NO2, observation.aqi_NO2 AS aqi_NO2, \
                 observation.firstMH_CO AS firstMH_CO, observation.firstMV_CO AS firstMV_CO, observation.mean_CO AS mean_CO, observation.unit_CO AS unit_CO, observation.aqi_CO AS aqi_CO, \
-                observation.firstMH_SO2 AS firstMH_SO2, observation.firstMV_SO2 AS firstMV_SO2, observation.mean_SO2 AS mean_SO2, observation.unit_SO2 AS unit_SO2, observation.aqi_SO2 AS aqi_SO2", 
-                {state_list: states, year_list: years});
+                observation.firstMH_SO2 AS firstMH_SO2, observation.firstMV_SO2 AS firstMV_SO2, observation.mean_SO2 AS mean_SO2, observation.unit_SO2 AS unit_SO2, observation.aqi_SO2 AS aqi_SO2\
+                SKIP toInteger($skipNumber) LIMIT toInteger($limitNumber)",
+                {state_list: states, min_year: interval.min, max_year: interval.max, skipNumber:skip, limitNumber:lines});
       return res.records;
   } catch (e) {
     console.log(e);
@@ -76,16 +81,34 @@ router.get('/', function(req, res) {
 });
 
 router.get('/filter', (req, res) => {
-  /*if (!req.body) {
+  if (!req.body) {
     res.status(400);
     res.json({message: "Bad Request"});
-  } else { */
-    let states = ['California', 'Arizona'];
-    let years = [2000, 2001];
-    filter_request(states, years).then((records) => {
-      res.send(records[0].toObject()); // to change
-    });
-  //}
+  } else {
+      if (!req.body) {
+          res.status(400);
+          res.json({message: "Bad Request"});
+      } else {
+          let states = req.body.states || ['California', 'Arizona'];
+          // page is number of page in table
+          // and lines is number of lines on the page
+          // there is no control over pages and lines being out of rage
+          let page = req.body.page || 100;
+          let lines = req.body.lines || 1;
+          let interval = req.body.interval;
+          if (!interval) {
+              db_info.get_years().then((records) => {
+                  interval = {
+                      min: records[0].get("min_year").toInt(),
+                      max: records[0].get("max_year").toInt()
+                  };
+                  filter_request(states, interval, page, lines).then((records) => {
+                      res.send(records); // to change
+                  });
+              });
+          }
+      }
+  }
 });
 
 router.get('/map', (req, res) => {
@@ -106,7 +129,6 @@ router.get('/map', (req, res) => {
                 res.send(map_data);
             });
         });
-        return;
     }
     }
 });
@@ -130,7 +152,6 @@ router.get('/years', (req, res) => {
     res.send(year_range);
   });
 });
-
 
 router.get('/location', (req, res) => {
     db_info.get_states_location().then((location) => {
