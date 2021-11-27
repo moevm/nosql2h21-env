3a39366ddd605bf2a7e720dffa18fc2f3aa25c46
a-kuzina <a.m.kuzina@gmail.com>
2021-11-27 19:43:25 +0300

    Might have issues! Added node module apoc for geocoding and nedeed surroundings. Example for apoc query (geocoding) without return interpretation

--- application/back/routes/extras/db_info.js
+++ application/back/routes/extras/db_info.js
@@ -1,5 +1,15 @@
 let neo4j = require('neo4j-driver');
 let creds = require("./credentials.json");
+var env = require('node-env-file');
+env(__dirname + '/.env');
+// YOU HAVE TO CREATE .env file in current directory with following content:
+//NEO4J_PROTOCOL=http
+// NEO4J_HOST=127.0.0.1
+// NEO4J_PORT=7474
+// NEO4J_USERNAME=neo4j
+// NEO4J_PASSWORD=your neo4j password
+let apoc = require('apoc');
+
 
 async function get_states() {
     let driver = neo4j.driver("neo4j://localhost", neo4j.auth.basic(creds.user, creds.password));
@@ -25,10 +35,10 @@ async function get_states_location() {
             try {
                 let res = await session.run(`MATCH (location:Address)\
                 WHERE location.state = $state\
-                RETURN DISTINCT location.address AS address, location.county AS county, location.city AS city`,
+                RETURN DISTINCT location.city AS city`,
                 {state: state});
                 if (res.records.length > 0) {
-                    location[state] = res.records[0].get("address") +" "+ state +" "+ res.records[0].get("county") +" "+ res.records[0].get("city");
+                    location[state] = res.records[0].get("city") +", USA";
                 }
             } catch(e) {
                 console.log(e);
@@ -40,6 +50,57 @@ async function get_states_location() {
         return location;
     });
     return location;
+}
+
+async function get_states_geolocation() {
+        let state = 'Arizona'
+        console.log(state);
+        let geolocation = get_states_location().then(async (location) => {
+            let driver = neo4j.driver("neo4j://localhost", neo4j.auth.basic(creds.user, creds.password));
+            let geolocation = {};
+            let session = driver.session();
+            console.log(location[state]);
+            try {
+
+                let res = apoc.query('CALL apoc.spatial.geocodeOnce(\'%locate%\') YIELD location RETURN location.latitude AS latitude, location.longitude AS longitude',  {locate: location[state].toString() }).exec().then(
+                    function (response) {
+                        console.log(1);
+                        console.log(response[0]);
+                        //console.log(response.data.toObject());
+                        //console.log(response.data[0]);
+                        //console.log(response.get('data'));
+                        //console.log(response.get(data));
+                        console.log(1);
+                    },
+                    function (fail) {
+                        console.log(2);
+                        console.log(fail);
+                    }
+                    //geolocation[state] = res.records[0].get("latitude") + " " + res.records[0].get("longitude");
+                );
+
+
+                console.log(3);
+                console.log(res);
+                geolocation[state] = 1;//res.records[0].get("latitude") + " " + res.records[0].get("longitude");
+                //console.log(geolocation[state]);
+                /*let res = await session.run(`CALL apoc.spatial.geocodeOnce('$location')\
+                YIELD location \
+                RETURN location.latitude AS latitude, location.longitude AS longitude`,
+                    {location: location[state]});
+
+                geolocation[state] = res.records[0].get("latitude") + " " + res.records[0].get("longitude");
+                console.log(geolocation[state]);*/
+            } catch (e) {
+                console.log(e);
+            } finally {
+                await session.close();
+            }
+                await driver.close();
+                return geolocation;
+        });
+        return geolocation;
+
 }
 
 
@@ -59,4 +120,5 @@ async function get_years() {
 
 exports.get_states= get_states;
 exports.get_states_location= get_states_location;
+exports.get_states_geolocation= get_states_geolocation;
 exports.get_years = get_years;
--- application/back/routes/index.js
+++ application/back/routes/index.js
@@ -138,5 +138,12 @@ router.get('/location', (req, res) => {
     });
 });
 
+router.get('/geolocation', (req, res) => {
+    db_info.get_states_geolocation().then((geolocation) => {
+        res.send(geolocation);
+
+    });
+});
+
 
 module.exports = router;
