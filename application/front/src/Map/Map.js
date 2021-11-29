e14be56a976432134cb47f96a1de848e307d7567
Maria Kulachkova <mariakulachk@mail.ru>
2021-11-30 00:27:27 +0300

    added some kind of map data output

--- application/front/src/Map/Map.js
+++ application/front/src/Map/Map.js
@@ -25,6 +25,43 @@ class Map extends Component {
         }
     }
 
+    async get_map_data() {
+        let interval = this.years
+        if (interval !== undefined) {
+            interval = {
+                min: interval.current_min,
+                max: interval.current_max
+            }
+        }
+        let substance;
+        for (const key in this.state.substances) {
+            if (this.state.substances[key]) {
+                substance = key;
+                break;
+            }
+        }
+
+        let states_data = {}
+        $.get('/location', {}, (res) => {
+            for (const key in res) {
+                states_data[key] = {address: res[key]}
+            }
+        });
+        await $.get('/map', {substance: substance, interval: interval}, (res) => {
+            for (const key in res) {
+                states_data[key].mean = res[key]
+            }
+        });
+
+        let res_str = "";
+        for (const key in states_data) {
+            res_str += `<p>${key}: {address: ${states_data[key]['address']}, mean: ${states_data[key]['mean']}}</p>`
+        }
+        $("#map-container").html(res_str);
+
+        this.states_data = states_data;
+    }
+
     componentDidMount() {
         $.get('/years', {}, (res) => {
             let years = res
@@ -33,11 +70,13 @@ class Map extends Component {
             this.years = years
             this.forceUpdate()
         })
+        this.get_map_data();
     }
 
     updateYears(current_min, current_max) {
         this.years.current_min = current_min
         this.years.current_max = current_max
+        this.get_map_data()
     }
 
     updateSubstances(event) {
@@ -53,6 +92,7 @@ class Map extends Component {
         }
         substances[event.target.value] = true
         this.setState({substances: substances})
+        this.get_map_data();
     }
 
     render() {
